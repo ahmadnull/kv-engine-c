865b6c394e48d3b9eb9b324fdb0b7ba92ef92d18
Ahmad Asaad <ahmadasaadh@gmail.com>
2026-09-22 15:34:18 +0300

    feat(app): json support

--- app/src/main.c
+++ app/src/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <argparse.h>
@@ -11,9 +12,27 @@ struct cmd_struct {
 };
 
 static const char *const cmd_put_usages[] = {
-    "kv-engine put --file | -f <file> <key> <value>",
+    "kv-map put --file | -f <file> <key> <value>",
     NULL,
 };
+
+/* == Helpers == */
+
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
+
+static bool is_ext_db(const char *filename) {
+    return (strcmp(get_filename_ext(filename), "db") == 0);
+}
+
+static bool is_ext_json(const char *filename) {
+    return (strcmp(get_filename_ext(filename), "json") == 0);
+}
+
+/* == Commands Functions == */
 
 int cmd_put(int argc, const char **argv) {
     const char *file = NULL;
@@ -27,24 +46,37 @@ int cmd_put(int argc, const char **argv) {
     argparse_describe(&argparse, "\nPut key-value pair into the database", NULL);
     argc = argparse_parse(&argparse, argc, argv);
 
-    KVEHashMap *engine = kve_map_create(KVE_DEFAULT_CAPACITY);
-    kve_db_load(engine, file);
+    KVEHashMap *map = kve_map_create(KVE_DEFAULT_CAPACITY);
+    if (is_ext_db(file)) {
+        kve_db_load(map, file);
+    }
+    else if (is_ext_json(file)) {
+        kve_json_load(map, file);
+    }
+    else return 1;
 
     const char *key = argv[0];
     const char *value = argv[1];
 
-    if(!kve_map_put(engine, key, value))
-        return 1;
-    if(!kve_db_save(engine, file))
+    if(!kve_map_put(map, key, value))
         return 1;
     
-    kve_map_destroy(engine);
+    if (is_ext_db(file)) {
+        if(!kve_db_save(map, file))
+            return 1;
+    }
+    else if (is_ext_json(file)) {
+        if(!kve_json_save(map, 0, file))
+            return 1;
+    }
+    
+    kve_map_destroy(map);
     
     return 0;
 }
 
 static const char *const cmd_get_usages[] = {
-    "kv-engine get --file | -f <file> <key>",
+    "kv-map get --file | -f <file> <key>",
     NULL,
 };
 
@@ -60,22 +92,29 @@ int cmd_get(int argc, const char **argv) {
     argparse_describe(&argparse, "\nGet value of key from the database", NULL);
     argc = argparse_parse(&argparse, argc, argv);
 
-    KVEHashMap *engine = kve_map_create(KVE_DEFAULT_CAPACITY);
-    if(!kve_db_load(engine, file))
-        return 1;
+    KVEHashMap *map = kve_map_create(KVE_DEFAULT_CAPACITY);
+    if (is_ext_db(file)) {
+        if(!kve_db_load(map, file))
+            return 1;
+    }
+    else if (is_ext_json(file)) {
+        if(!kve_json_load(map, file))
+            return 1;
+    }
+    else return 1;
 
     const char *key = argv[0];
-    const char *value = kve_map_get(engine, key);
+    const char *value = kve_map_get(map, key);
 
-    printf("%s:\t%s\n", key, value);
+    printf("{\"%s\": \"%s\"}\n", key, value);
 
-    kve_map_destroy(engine);
+    kve_map_destroy(map);
 
     return 0;
 }
 
 static const char *const cmd_rm_usages[] = {
-    "kv-engine rm --file | -f <file> <key>",
+    "kv-map rm --file | -f <file> <key>",
     NULL,
 };
 
@@ -91,32 +130,48 @@ int cmd_rm(int argc, const char **argv) {
     argparse_describe(&argparse, "\nRemove key-value pair from the database", NULL);
     argc = argparse_parse(&argparse, argc, argv);
 
-    KVEHashMap *engine = kve_map_create(KVE_DEFAULT_CAPACITY);
-    if(!kve_db_load(engine, file))
-        return 1;
+    KVEHashMap *map = kve_map_create(KVE_DEFAULT_CAPACITY);
+    if (is_ext_db(file)) {
+        if(!kve_db_load(map, file))
+            return 1;
+    }
+    else if (is_ext_json(file)) {
+        if(!kve_json_load(map, file))
+            return 1;
+    }
+    else return 1;
 
     const char *key = argv[0];
 
-    if(!kve_map_remove(engine, key))
-        return 1;
-    if(!kve_db_save(engine, file))
+    if(!kve_map_remove(map, key))
         return 1;
     
-    kve_map_destroy(engine);
+    if (is_ext_db(file)) {
+        if(!kve_db_save(map, file))
+            return 1;
+    }
+    else if (is_ext_json(file)) {
+        if(!kve_json_save(map, 0, file))
+            return 1;
+    }
+    
+    kve_map_destroy(map);
 
     return 0;
 }
 
 static const char *const cmd_show_usages[] = {
-    "kv-engine show --file | -f <file>",
+    "kv-map show --file | -f <file> --indentation | -i <indentation>",
     NULL,
 };
 
 int cmd_show(int argc, const char **argv) {
     const char *file = NULL;
+    size_t indentation = 4;
     struct argparse_option options[] = {
         OPT_HELP(),
         OPT_STRING('f', "file", &file, "file to load/save", NULL, 0, 0),
+        OPT_INTEGER('i', "indentation", &indentation, "JSON indentation (default 4)", NULL, 0, 0),
         OPT_END(),
     };
     struct argparse argparse;
@@ -124,27 +179,28 @@ int cmd_show(int argc, const char **argv) {
     argparse_describe(&argparse, "\nShow all key-value pairs from the database", NULL);
     argc = argparse_parse(&argparse, argc, argv);
 
-    KVEHashMap *engine = kve_map_create(KVE_DEFAULT_CAPACITY);
-    if(!kve_db_load(engine, file))
-        return 1;
-
-    KVEIterator *iter = kve_iter_create(engine);
-    if(!iter) {
-        kve_map_destroy(engine);
-        return 1;
+    KVEHashMap *map = kve_map_create(KVE_DEFAULT_CAPACITY);
+    if (is_ext_db(file)) {
+        if(!kve_db_load(map, file))
+            return 1;
     }
-    
-    while(kve_iter_next(iter))
-        printf("%s:\t%s\n", kve_iter_key(iter), kve_iter_value(iter));
+    else if (is_ext_json(file)) {
+        if(!kve_json_load(map, file))
+            return 1;
+    }
+    else return 1;
 
-    kve_map_destroy(engine);
-    kve_iter_destroy(iter);
+    char *json = kve_json_serialize(map, indentation);
+    printf("%s", json);
+
+    kve_map_destroy(map);
+    free(json);
 
     return 0;
 }
 
 static const char *const cmd_contains_usages[] = {
-    "kv-engine contains --file | -f <file> <key>",
+    "kv-map contains --file | -f <file> <key>",
     NULL,
 };
 
@@ -152,7 +208,7 @@ int cmd_contains(int argc, const char **argv) {
     const char *file = NULL;
     struct argparse_option options[] = {
         OPT_HELP(),
-        OPT_STRING('f', "file", &file, "file to load/save", NULL, 0, 0),
+        OPT_STRING('f', "file", &file, ".json/.db file to load/save (extension must be either of two)", NULL, 0, 0),
         OPT_END(),
     };
     struct argparse argparse;
@@ -160,15 +216,22 @@ int cmd_contains(int argc, const char **argv) {
     argparse_describe(&argparse, "\nCheck if key is in the database", NULL);
     argc = argparse_parse(&argparse, argc, argv);
 
-    KVEHashMap *engine = kve_map_create(KVE_DEFAULT_CAPACITY);
-    if(!kve_db_load(engine, file))
-        return 1;
+    KVEHashMap *map = kve_map_create(KVE_DEFAULT_CAPACITY);
+    if (is_ext_db(file)) {
+        if(!kve_db_load(map, file))
+            return 1;
+    }
+    else if (is_ext_json(file)) {
+        if(!kve_json_load(map, file))
+            return 1;
+    }
+    else return 1;
 
     const char *key = argv[0];
 
-    int result = kve_map_contains(engine, key) ? 0 : 1;
+    int result = kve_map_contains(map, key) ? 0 : 1;
 
-    kve_map_destroy(engine);
+    kve_map_destroy(map);
 
     return result;
 }
@@ -182,7 +245,7 @@ static struct cmd_struct commands[] = {
 };
 
 static const char *const usages[] = {
-    "kv-engine [options] [cmd] [args]\n"
+    "kv-map [options] [cmd] [args]\n"
     "Available subcommands:\n"
     "\n"
     "\tput\n"
